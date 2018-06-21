7154bfcebb487fdd6b015c436778c5945b9b789d
hao.duan <hao.duan@newtouch.cn>
2018-06-21 22:09:38 +0800

    日志修改

--- src/main/java/com/newtouch/blog/controller/OpenNoLoginController.java
+++ src/main/java/com/newtouch/blog/controller/OpenNoLoginController.java
@@ -5,6 +5,7 @@ import java.text.ParseException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,10 +18,11 @@ import com.newtouch.common.util.DateUtil;
 @Controller
 @RequestMapping("/openJsp")
 public class OpenNoLoginController { 
+	 public static Logger logonlc = Logger.getLogger(OpenNoLoginController.class);  
 	@RequestMapping("/openIndex")
     public String openIndex(HttpServletRequest request,Model model){ 
 		try {
-			System.out.println(CookieUtil.getIpAddr(request) + DateUtil.getCurrDate() +"index");
+			logonlc.info("访问ID地址："+CookieUtil.getAddress("ip=" + CookieUtil.getIpAddr(request), "utf-8") + "访问ID地址："+CookieUtil.getIpAddr(request) + "访问时间："+DateUtil.getCurrDate() +"，访问界面：index");
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -33,7 +35,7 @@ public class OpenNoLoginController {
 	@RequestMapping("/about-us")
     public String aboutUs(HttpServletRequest request,Model model){ 
 		try {
-			System.out.println("访问ID地址："+CookieUtil.getIpAddr(request) + "访问时间："+DateUtil.getCurrDate() +"about-us");
+			logonlc.info("访问ID地址："+CookieUtil.getAddress("ip=" + CookieUtil.getIpAddr(request), "utf-8") + "访问ID地址："+CookieUtil.getIpAddr(request) + "访问时间："+DateUtil.getCurrDate() +"，访问界面：about-us");
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -46,7 +48,7 @@ public class OpenNoLoginController {
 	@RequestMapping("/services")
     public String services(HttpServletRequest request,Model model){ 
 		try {
-			System.out.println("访问ID地址："+CookieUtil.getIpAddr(request) + "访问时间："+DateUtil.getCurrDate() +"services");
+			logonlc.info("访问ID地址："+CookieUtil.getAddress("ip=" + CookieUtil.getIpAddr(request), "utf-8") + "访问ID地址："+CookieUtil.getIpAddr(request) + "访问时间："+DateUtil.getCurrDate() +"，访问界面：services");
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -59,7 +61,7 @@ public class OpenNoLoginController {
 	@RequestMapping("/portfolio")
     public String portfolio(HttpServletRequest request,Model model){ 
 		try {
-			System.out.println("访问ID地址："+CookieUtil.getIpAddr(request) + "访问时间："+DateUtil.getCurrDate() +"portfolio");
+			logonlc.info("访问ID地址："+CookieUtil.getAddress("ip=" + CookieUtil.getIpAddr(request), "utf-8") + "访问ID地址："+CookieUtil.getIpAddr(request) + "访问时间："+DateUtil.getCurrDate() +"，访问界面：portfolio");
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -72,7 +74,7 @@ public class OpenNoLoginController {
 	@RequestMapping("/contact-us")
     public String contactUs(HttpServletRequest request,Model model){ 
 		try {
-			System.out.println("访问ID地址："+CookieUtil.getIpAddr(request) + "访问时间："+DateUtil.getCurrDate() +"contact-us");
+			logonlc.info("访问ID地址："+CookieUtil.getAddress("ip=" + CookieUtil.getIpAddr(request), "utf-8") + "访问ID地址："+CookieUtil.getIpAddr(request) + "访问时间："+DateUtil.getCurrDate() +"，访问界面：contact-us");
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -82,4 +84,18 @@ public class OpenNoLoginController {
 		}
         return "contact-us";    
     }
+	@RequestMapping("/pricing")
+    public String pricing(HttpServletRequest request,Model model){ 
+		try {
+			logonlc.info("访问ID地址："+CookieUtil.getAddress("ip=" + CookieUtil.getIpAddr(request), "utf-8") + "访问ID地址："+CookieUtil.getIpAddr(request) + "访问时间："+DateUtil.getCurrDate() +"，访问界面：pricing");
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        return "pricing";    
+    }
 }
+
